0911fadd042097bf90b897d7fa95cd1df7a73391
ChocPanda <mattsearle@ymail.com>
2017-10-14 12:31:02 +0100

    Exercise 2.2

--- src/test/scala/org/panda/arrays/Ex2-2.scala
+++ src/test/scala/org/panda/arrays/Ex2-2.scala
@@ -5,10 +5,15 @@ import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.panda.tags._
 
-class ArraySpec extends FreeSpec with Matchers with PropertyChecks {
+class ArraySpec extends FreeSpec with Matchers {
   "The Sorted object" - {
-    "isSorted should return true for a simple sequence" in {
+    "isSorted should return true for a simple sequence of integers" in {
         Sorted.isSorted(Array(1, 2, 3), (a: Int, b: Int) => a <= b) shouldBe true
+        Sorted.isSorted(Array(3, 2, 1), (a: Int, b: Int) => a >= b) shouldBe true
+    }
+
+    "isSorted should return true for an empty array" in {
+        Sorted.isSorted(Array[Int](), (a: Int, b: Int) => a <= b) shouldBe true
     }
   }
 }
