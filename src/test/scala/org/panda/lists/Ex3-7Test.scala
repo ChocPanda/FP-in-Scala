2b60cceeb7bb07b8c15940410d2694295a4db4cf
ChocPanda <mattsearle@ymail.com>
2017-10-14 19:06:28 +0100

    Add pointless comment that nobody will read

--- src/test/scala/org/panda/lists/Ex3-7Test.scala
+++ src/test/scala/org/panda/lists/Ex3-7Test.scala
@@ -5,6 +5,12 @@ import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 
 class MyListTests extends FreeSpec with Matchers with PropertyChecks {
+
+    /**
+    This is unused I just wanted to try and write it. It could be improved by increasing the probablity
+    of having longer lists or by better integrating my list with the scala.Collections library by implementing
+    TraversableLike, etc... so scalacheck could simply use Gen.containerOf, however that felt wildly out of scope.
+     **/     
     implicit def myListGen[A](implicit aGen: Gen[A], lGen: Gen[MyList[A]]) = for {
         head <- aGen
         tail <- lGen
@@ -19,6 +25,7 @@ class MyListTests extends FreeSpec with Matchers with PropertyChecks {
             }
         }
 
+    "MyList.product should" - {
         "return the product of a scala.collections.List[Int]" in {
             forAll { list: List[Int] =>
                 val myList = MyList.apply(list: _*)
