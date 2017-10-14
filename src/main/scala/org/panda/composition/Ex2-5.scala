5baf7093d874ce1661b837f8374d7a487eb7f226
ChocPanda <mattsearle@ymail.com>
2017-10-14 13:10:43 +0100

    Add first principles solution to Exercise 2.5

--- src/main/scala/org/panda/composition/Ex2-5.scala
+++ src/main/scala/org/panda/composition/Ex2-5.scala
@@ -2,4 +2,6 @@ package org.panda.composition
 
  object Composition extends App {
      def compose[A, B, C](f: B => C, g: A => B): A => C = g andThen f
+
+     def firstPrinciplesCompose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
  }
