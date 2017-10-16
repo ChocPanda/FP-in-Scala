0d4e4da7bdb812489c2082d3958c22bbb6623cda
ChocPanda <mattsearle@ymail.com>
2017-10-16 21:21:29 +0100

    Reorganise List tests to flatspec

--- src/test/scala/org/panda/lists/Ex3-7Test.scala
+++ src/test/scala/org/panda/lists/Ex3-7Test.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 
-class MyListTests extends FreeSpec with Matchers with PropertyChecks {
+class MyListTests extends FlatSpec with Matchers with PropertyChecks {
 
     /**
     This is unused I just wanted to try and write it. It could be improved by increasing the probablity
@@ -17,20 +17,21 @@ class MyListTests extends FreeSpec with Matchers with PropertyChecks {
         myList <- Gen.oneOf(MyNil, Cons(head, MyNil), Cons(head, tail))
     } yield myList
 
-    "MyList.sum should" - {
-        "match the sum of a scala.collections.List[Int]" in {
-            forAll { list: List[Int] =>
-                val myList = MyList.apply(list: _*)
-                myList.sum shouldEqual list.sum
-            }
+    "MyList.sum" should "match the sum of a scala.collections.List[Int]" in {
+        forAll { list: List[Int] =>
+            val myList = MyList.apply(list: _*)
+            myList.sum shouldEqual list.sum
         }
+    }
 
-    "MyList.product should" - {
-        "return the product of a scala.collections.List[Int]" in {
-            forAll { list: List[Int] =>
-                val myList = MyList.apply(list: _*)
-                myList.product shouldEqual list.fold(1)(_ * _)
-            }
+    "MyList.product" should "return the product of a scala.collections.List[Int]" in {
+        forAll { list: List[Int] =>
+            val myList = MyList.apply(list: _*)
+            myList.product shouldEqual list.fold(1)(_ * _)
         }
+    }
+
+    "test" should "demonstrate Exercise 3.8" in {
+        println(MyList.foldRight(MyList(1, 2, 3), MyNil: MyList[Int])(Cons(_, _)))
     }
 }
